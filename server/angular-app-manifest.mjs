4ed63c67ebe2d2e1f9e298e04d4ddeba4fd2a87d
Brandon22Adams <41342071+Brandon22Adams@users.noreply.github.com>
2025-11-20 11:19:54 -0700

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,24 +2,24 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/Portfolio/',
+  baseHref: '/portfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Portfolio"
+    "route": "/portfolio"
   },
   {
     "renderMode": 2,
-    "route": "/Portfolio/about-me"
+    "route": "/portfolio/about-me"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 12864, hash: 'e2ee459bf22fb92fba209239cf04f1f8569b8727148f5bdba2884bf4689cd6a9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3683, hash: 'd5d5e9b211535f393eafbb396ec526dd307058e2d1162319b2c9996583ea1dd5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 15245, hash: '12bb6ab6b24f3b854a75bf9530606bb8193b409d6fb0abba0846ad52e434c4b7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'about-me/index.html': {size: 15247, hash: '1deb2c935af568641e13abd7bfd10aaf8e9395465c41cc7a29c03e4c9f105ddb', text: () => import('./assets-chunks/about-me_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 12864, hash: 'e780c1aac7b5d2b1523205d724c170e7ae2a75937f4f3717921b8a9aac0ff5a2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3683, hash: '01e45af353be4d71ea1dbc3d6c2978764038d65ac77e0e3eb5924b6c1f0256c7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 15245, hash: 'dc403034ea1b09a4dd0dd15417875e28d8feb295056862729e3867b224e22710', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'about-me/index.html': {size: 15247, hash: 'e1e9ad8fd2dcb282c514e8b30e109b53c5905406bdbcf04fa77c5df549c56289', text: () => import('./assets-chunks/about-me_index_html.mjs').then(m => m.default)},
     'styles-FGVEQUSU.css': {size: 13088, hash: 'cwRKFzxfpro', text: () => import('./assets-chunks/styles-FGVEQUSU_css.mjs').then(m => m.default)}
   },
 };
